3346dafd8888418b534ca93d4b6bbeb26cbc9e37
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-06-11 11:45:27 -0500

    feat: :children_crossing: add metadata

---
+++ src/app/metadata.ts
@@ -0,0 +1,7 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+	title: 'Glossary',
+	description:
+		'Glossary is an online dictionary providing comprehensive definitions, phonetics, and synonyms for English words.',
+}
