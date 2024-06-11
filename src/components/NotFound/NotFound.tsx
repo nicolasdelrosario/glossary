9210427cc11c703645df8a82d5d115e6daa0e080
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-06-11 11:09:40 -0500

    feat: :sparkles: add NotFound component

---
+++ src/components/NotFound/NotFound.tsx
@@ -0,0 +1,8 @@
+import styles from './styles.module.css'
+export default function NotFound() {
+	return (
+		<section className={styles.not_found}>
+			<h2>This word does not exist.</h2>
+		</section>
+	)
+}
